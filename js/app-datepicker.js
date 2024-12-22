ad5eb285670056f75ddcaff60f0eee1bfb916d6e
nikonenkoincodeit <75611895+nikonenkoincodeit@users.noreply.github.com>
2024-12-22 20:36:06 +0200

    update style datepicker

--- js/app-datepicker.js
+++ js/app-datepicker.js
@@ -1,54 +1,96 @@
-const fla = flatpickr("#datepicker", {
-  appendTo: "#datepicker",
-  inline: true,
-  locale: "pl",
-  minDate: "today",
-  conjunction: "multiple",
-  disableMobile: true,
-  showMonths: 2,
-  dateFormat: "Y-m-d",
-  mode: "range",
-  onReady: function (selectedDates, dateStr, instance) {
-    updateFlatpickr();
-  },
-  onChange: function (selectedDates) {
-    console.log("selectedDates ", selectedDates);
-    updateFlatpickr();
-  },
-});
-
-console.log("fla ", fla);
-
-window.addEventListener("load", () => {
-  const screenWidth = window.innerWidth;
-  if (screenWidth > 991) {
-  } else {
+class Datepicker {
+  constructor({ showMonthsMob = 12, showMonthsDes = 2, locale = "pl", appendTo = "#datepicker", flatpickr = null, screenWidth = 991 } = {}) {
+    this.showMonths = 2;
+    this.showMonthsMob = showMonthsMob;
+    this.showMonthsDes = showMonthsDes;
+    this.screenWidth = screenWidth;
+    this.flatpickr = flatpickr;
+    this.appendTo = appendTo;
+    this.locale = locale;
+    this.instance = null;
+    this.flagM = false;
+    this.flagD = false;
   }
-});
-
-window.addEventListener("resize", () => {
-  const screenWidth = window.innerWidth;
-  if (screenWidth > 991) {
-    fla.destroy();
+  get isMob() {
+    return window.innerWidth < this.screenWidth;
   }
-});
-
-function updateFlatpickr() {
-  const screenWidth = window.innerWidth;
-  if (screenWidth > 991) return;
-  const flatpickr = document.querySelector(".flatpickr-calendar");
-  const dayContainer = flatpickr.querySelectorAll(".dayContainer");
-  const flatpickrMonth = flatpickr.querySelectorAll(".flatpickr-month");
-  const datepickerHeader = document.querySelector(".js-app-datepicker-header");
-
-  const weekdaycontainer = flatpickr.querySelector(".flatpickr-weekdaycontainer").cloneNode(true);
-
-  if (datepickerHeader.querySelector(".flatpickr-weekdaycontainer")) {
-    datepickerHeader.querySelector(".flatpickr-weekdaycontainer").remove();
+  createFlatpickr() {
+    let _this = this;
+    this.instance = this.flatpickr("#datepicker", {
+      appendTo: "#datepicker",
+      inline: true,
+      locale: this.locale,
+      minDate: "today",
+      conjunction: "multiple",
+      disableMobile: true,
+      showMonths: this.showMonths,
+      dateFormat: "Y-m-d",
+      mode: "range",
+      onReady: function (selectedDates, dateStr, instance) {
+        console.log(this);
+        _this.updateFlatpickr();
+      },
+      onChange: function (selectedDates) {
+        console.log("selectedDates ", selectedDates);
+        // callback();
+        _this.updateFlatpickr();
+      },
+    });
   }
-  datepickerHeader.insertAdjacentElement("beforeend", weekdaycontainer);
+  init() {
+    this.createFlatpickr(this.updateFlatpickr.bind(this));
+  }
+  start() {
+    this.init();
+    window.addEventListener("resize", this.resize.bind(this));
+  }
+  destroy() {
+    if (!this.instance) return;
+    this.instance?.destroy();
+    this.instance = null;
+    // window.removeEventListener("resize", this.resize.bind(this));
+  }
+  resize() {
+    // if (this.isMob) {
+    //   this.showMonths = this.showMonthsMob;
+    // } else this.showMonths = this.showMonthsDes;
+    console.log(this);
+    if (this.isMob && !this.flagM) {
+      this.showMonths = this.isMob ? this.showMonthsMob : this.showMonthsDes;
+      this.destroy();
+      this.init();
+      this.flagM = true;
+      this.flagD = false;
+    }
 
-  dayContainer.forEach((target, index) => {
-    target.insertAdjacentElement("beforebegin", flatpickrMonth[index].cloneNode(true));
-  });
+    if (!this.isMob && !this.flagD) {
+      this.showMonths = this.isMob ? this.showMonthsMob : this.showMonthsDes;
+      this.destroy();
+      this.init();
+      this.flagD = true;
+      this.flagM = false;
+    }
+  }
+
+  updateFlatpickr() {
+    if (!this.isMob) return;
+    const flatpickr = document.querySelector(".flatpickr-calendar");
+    const dayContainer = flatpickr.querySelectorAll(".dayContainer");
+    const flatpickrMonth = flatpickr.querySelectorAll(".flatpickr-month");
+    const datepickerHeader = document.querySelector(".js-app-datepicker-header");
+
+    const weekdaycontainer = flatpickr.querySelector(".flatpickr-weekdaycontainer").cloneNode(true);
+
+    if (datepickerHeader.querySelector(".flatpickr-weekdaycontainer")) {
+      datepickerHeader.querySelector(".flatpickr-weekdaycontainer").remove();
+    }
+    datepickerHeader.insertAdjacentElement("beforeend", weekdaycontainer);
+
+    dayContainer.forEach((target, index) => {
+      target.insertAdjacentElement("beforebegin", flatpickrMonth[index].cloneNode(true));
+    });
+  }
 }
+
+const datepicker = new Datepicker({ flatpickr });
+datepicker.start();
